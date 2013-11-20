429bb3338ea38ed741ae50e524751723530bbd6b
Clark Fitzgerald <clarkfitzg@gmail.com>
2013-11-20 07:21:12 -0800

    starting with python

---
+++ load_ti.py
@@ -0,0 +1,10 @@
+'''
+Clark - 20 Nov 13
+Load and clean the Titanic dataset.
+'''
+
+import pandas as pd
+
+data = pd.read_csv('/Users/clark/data/titanic/train.csv')
+
+# data.dtypes checks type of each column
